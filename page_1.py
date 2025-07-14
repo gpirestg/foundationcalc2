95e9201fa410f3234185566da4a441e5ba954dd4
gpirestg <gilberto.pires@tonygee.com>
2025-07-14 12:44:57 +0100

    Update page_1.py

--- page_1.py
+++ page_1.py
@@ -79,11 +79,11 @@ def run():
             ###################################################################################################################
             # File paths and global variables
             #file_path = r'C:\Users\GDP\OneDrive - Tony Gee and Partners LLP\Documents\Automation\__2025__Digital__\DEV - WIP\P&E Streamlit Toms App\structural_app2\loads_template'
-            file_path = "/mount/src/foundationcalc/structural_app2/loads_template/"
+            file_path = "/mount/src/foundationcalc2/loads_template/"
             #loads_file_path = r'C:\Users\GDP\OneDrive - Tony Gee and Partners LLP\Documents\Automation\__2025__Digital__\DEV - WIP\P&E Streamlit Toms App\Rev01\foundation_loadsv2.xlsx'
             loads_file_path = os.path.join(file_path, template)
             #optimised_output_path = r'C:\Users\GDP\OneDrive - Tony Gee and Partners LLP\Documents\Automation\__2025__Digital__\DEV - WIP\P&E Streamlit Toms App\structural_app2\output\optimised_result.xlsx'
-            optimised_output_path = "/mount/src/foundationcalc/structural_app2/output/optimised_result.xlsx"
+            optimised_output_path = "/mount/src/foundationcalc2/output/optimised_result.xlsx"
             #loads_sheet_name = 'Loading Summary'
             loads_sheet_name = sheet
             ###################################################################################################################
